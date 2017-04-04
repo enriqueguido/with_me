b0103c1ed4478e5a88a7892ae88dc6f738b215ac
Max <enrique.guido@yahoo.com>
2017-04-04 17:24:51 -0400

    Created a model

---
+++ db/migrate/20170404212319_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password
+      t.string :gender
+      t.integer :event_id
+
+      t.timestamps
+    end
+  end
+end
