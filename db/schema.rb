890a1dc69e8f92a5718ec934c8ebb5fd848be90f
Max <enrique.guido@yahoo.com>
2017-04-04 18:11:37 -0400

    Changed databased to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170404213557) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text     "comment_body"
     t.datetime "date"
