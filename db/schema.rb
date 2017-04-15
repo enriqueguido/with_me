b294337f96d71bed5723e2a5ff9983e8078e953a
KrisDowns <kdowns01@gmail.com>
2017-04-15 14:33:59 -0400

    Fixed search button and added topnav to index and event page.

--- db/schema.rb
+++ db/schema.rb
@@ -28,8 +28,8 @@ ActiveRecord::Schema.define(version: 20170414201223) do
     t.string   "name"
     t.string   "description"
     t.string   "tz"
-    t.date     "start"
-    t.date     "end"
+    t.datetime "start"
+    t.datetime "end"
     t.string   "logo"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
